d8506b73158ac18ad2780476fe93bfc0a6aa2dbb
scarlett2003 <jovilee@tpts8.seed.net.tw>
2015-05-15 01:46:44 +0800

    animation : rotation

---
+++ iosAnimationSample/RotationViewController.swift
@@ -0,0 +1,51 @@
+//
+//  RotationViewController.swift
+//  iosAnimationSample
+//
+//  Created by vialley-imac on 2015/5/14.
+//  Copyright (c) 2015年 ookiwi. All rights reserved.
+//
+
+import UIKit
+
+class RotationViewController: UIViewController {
+    @IBOutlet weak var wheel: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    func spin(){
+        UIView.animateWithDuration(1, delay: 0, options: .CurveLinear, animations: {
+            self.wheel.transform = CGAffineTransformRotate(self.wheel.transform, CGFloat(M_PI))
+        }) { (finished) -> Void in
+            self.spin()
+        }
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        self.spin()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
